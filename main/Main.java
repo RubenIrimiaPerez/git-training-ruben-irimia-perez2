f952574b3ba868486b04a1ad9be3cb13432d3421
Ruben <rubenbaloncesto10@gmail.com>
2023-10-30 10:45:16 +0100

    Clase main empezada

---
+++ main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Scanner;
+import thread.Thread;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        long[] resultados = new long[2];
+        
+        System.out.println("Introduzca cadena");
+        String cadena = sc.nextLine();
+        
+        
+        Hilo h1 = new Hilo(cadena,resultados,0,0, cadena.length()/2);
+        Hilo h2 = new Hilo(cadena,resultados,1,cadena.length()/2,cadena.length());
+        
+        h1.start();
+        h2.start();
